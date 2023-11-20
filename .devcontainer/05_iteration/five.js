78a5049530b7be2d2f3fbc6ebcc1adae231b7ae0
imharoon06 <114802569+imharoon06@users.noreply.github.com>
2023-11-20 23:31:56 +0000

    mycoding

---
+++ .devcontainer/05_iteration/five.js
@@ -0,0 +1,33 @@
+const coding =["java","python","ruby","cpp"]
+
+// coding.forEach( function(val){
+//     console.log(val);
+// })
+
+// coding.forEach( (item) => {
+// console.log(item);
+// })
+
+// function printMe (item,index,arr){
+// console.log(item,index,arr);
+// }
+// coding.forEach(printMe)
+
+const myCoding = [
+    { 
+    languageName: "javascript",
+    languageFileName: "js"
+},
+    { 
+    languageName: "java",
+    languageFileName: "java"
+},
+    { 
+    languageName: "python",
+    languageFileName: "py"
+},
+
+]
+coding.forEach( (item) => {
+    console.log(item.languageName);
+})
